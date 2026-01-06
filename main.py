9aad2b7926d6c6bf69560f674614bb87c885879c
KG1303 <claw75@yandex.ru>
2026-01-06 16:08:54 +0300

    Update main.py
    Organized imports in main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import asyncio
 import os
-from dotenv import load_dotenv
+
 import requests
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.types import Message, URLInputFile
-from aiogram.filters import CommandStart, Command
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message, URLInputFile
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -44,13 +44,16 @@ async def weather(message: Message):
     elif response <= -12:
         await message.answer("It's below -11°C outside, dress warmly")
 
+
 class WeatherStates(StatesGroup):
     waiting_for_city = State()
+
 
 @dp.message(Command("weather_city"))
 async def weather_city(message: Message, state: FSMContext):
     await message.answer("Please enter a city name")
     await state.set_state(WeatherStates.waiting_for_city)
+
 
 @dp.message(WeatherStates.waiting_for_city)
 async def get_weather_by_city(message: Message, state: FSMContext):
@@ -75,51 +78,56 @@ async def get_weather_by_city(message: Message, state: FSMContext):
     elif response <= -12:
         await message.answer("It's below -11°C outside, dress warmly")
 
+
 class TimerStates(StatesGroup):
     waiting_for_time = State()
 
+
 @dp.message(Command("timer"))
 async def timer_command(message: Message, state: FSMContext):
-    await message.answer('Ok, for which time should I start the timer? (format: h:m:s)')
+    await message.answer("Ok, for which time should I start the timer? (format: h:m:s)")
     await state.set_state(TimerStates.waiting_for_time)
+
 
 @dp.message(TimerStates.waiting_for_time)
 async def process_timer_time(message: Message, state: FSMContext):
     user_time = message.text
-    hours, minutes, seconds = map(int, user_time.split(':'))
-    total_seconds = hours * 3600 + minutes * 60 + seconds    
+    hours, minutes, seconds = map(int, user_time.split(":"))
+    total_seconds = hours * 3600 + minutes * 60 + seconds
     await message.answer("Timer is set")
     await asyncio.sleep(total_seconds)
-    await message.answer('End of the timer!')
+    await message.answer("End of the timer!")
     await state.clear()
 
+
 class PomodoroStates(StatesGroup):
-	continue_pomodoro = State()
+    continue_pomodoro = State()
+
 
 @dp.message(Command("pomodoro"))
 async def pomodoro(message: Message, state: FSMContext):
     c = 0
     while True:
-        await message.answer('Have a rest')
+        await message.answer("Have a rest")
         if c % 4 == 0 and c != 0:
             await asyncio.sleep(900)
         else:
             await asyncio.sleep(300)
         await message.answer("Pomodoro has started. Be productive!")
         await asyncio.sleep(1500)
-        await message.answer('Pomodoro just ended. Should we continue?')
+        await message.answer("Pomodoro just ended. Should we continue?")
         c += 1
         await state.set_state(PomodoroStates.continue_pomodoro)
 
+
 @dp.message(PomodoroStates.continue_pomodoro)
 async def conntinue_p(message: Message, state: FSMContext):
-	if message.text == "Yes":
-		await state.clear()
-		await pomodoro(message, state)
-	else:
-		await state.clear()
-		await message.answer('Good job!')
-
+    if message.text == "Yes":
+        await state.clear()
+        await pomodoro(message, state)
+    else:
+        await state.clear()
+        await message.answer("Good job!")
 
 
 async def main():
